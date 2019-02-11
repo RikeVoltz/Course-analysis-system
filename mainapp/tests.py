99d38baccdd61d053d2771f44881b4ce443f1550
RikeVoltz <rikevoltz@gmail.com>
2019-02-12 02:35:09 +0300

    'Fixup' failing test

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -6,7 +6,3 @@ class FirstTestCase(TestCase):
     def test_successful(self):
         """Test, that must finish successfully"""
         self.assertEqual(True, True)
-
-    def test_unsuccessful(self):
-        """Test, that must fail"""
-        self.assertEqual(False, True)
